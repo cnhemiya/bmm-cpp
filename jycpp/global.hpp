4130c9be4edd440135f5caba2d6132db8c3f43c3
cnhemiya <cnhemiya@qq.com>
2022-01-27 17:15:15 +0800

    增加 global.hpp

---
+++ jycpp/global.hpp
@@ -0,0 +1,10 @@
+#ifndef _JYCPP_GLOBAL_HPP_
+#define _JYCPP_GLOBAL_HPP_
+
+#ifndef _JYCPP_NAMESPACE_BEGIN_
+#define _JYCPP_NAMESPACE_BEGIN_ namespace jycpp {
+
+#ifndef _JYCPP_NAMESPACE_END_
+#define _JYCPP_NAMESPACE_END_ }
+
+#endif // _JYCPP_GLOBAL_HPP_
