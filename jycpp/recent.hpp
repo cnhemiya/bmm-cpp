a9abdd3bb9eb68f3b02f2a31d771337a67425423
cnhemiya <cnhemiya@qq.com>
2022-01-27 14:18:51 +0800

    修改 recent 内部命名符合 stl 习惯

--- jycpp/recent.hpp
+++ jycpp/recent.hpp
@@ -17,124 +17,124 @@ public:
     typedef typename datas_type::const_reverse_iterator const_reverse_iterator;
 
     recent(size_type __n = 10)
-        : _m_max_size(__n)
+        : _M_max_size(__n)
     {
     }
 
     recent(const std::initializer_list<value_type> &__il)
-        : _m_max_size(10)
+        : _M_max_size(10)
     {
         for (const auto &i : __il)
-            _m_datas.push_front(i);
+            _M_datas.push_front(i);
         resize(10);
     }
 
     void push(const value_type &__x)
     {
-        while (_m_datas.size() >= _m_max_size)
-            _m_datas.pop_back();
-        _m_datas.push_front(__x);
+        while (_M_datas.size() >= _M_max_size)
+            _M_datas.pop_back();
+        _M_datas.push_front(__x);
     }
 
     void pop()
     {
-        _m_datas.pop_back();
+        _M_datas.pop_back();
     }
 
     reference front() noexcept
     {
-        return *_m_datas.begin();
+        return *_M_datas.begin();
     }
 
     const_reference front() const noexcept
     {
-        return *_m_datas.begin();
+        return *_M_datas.begin();
     }
 
     reference back() noexcept
     {
-        iterator __t = _m_datas.end();
-        --__t;
-        return *__t;
+        iterator __tmp = _M_datas.end();
+        --__tmp;
+        return *__tmp;
     }
 
     const_reference back() const noexcept
     {
-        const_iterator __t = _m_datas.end();
-        --__t;
-        return *__t;
+        const_iterator __tmp = _M_datas.end();
+        --__tmp;
+        return *__tmp;
     }
 
     iterator begin() noexcept
     {
-        return _m_datas.begin();
+        return _M_datas.begin();
     }
 
     const_iterator begin() const
     {
-        return _m_datas.begin();
+        return _M_datas.begin();
     }
 
     iterator end() noexcept
     {
-        return _m_datas.end();
+        return _M_datas.end();
     }
 
     const_iterator end() const noexcept
     {
-        return _m_datas.end();
+        return _M_datas.end();
     }
 
     iterator rbegin() noexcept
     {
-        return _m_datas.rbegin();
+        return _M_datas.rbegin();
     }
 
     const_iterator rbegin() const noexcept
     {
-        return _m_datas.rbegin();
+        return _M_datas.rbegin();
     }
 
     iterator rend() noexcept
     {
-        return _m_datas.rend();
+        return _M_datas.rend();
     }
 
     const_iterator rend() const noexcept
     {
-        return _m_datas.rend();
+        return _M_datas.rend();
     }
 
     size_type size() const noexcept
     {
-        return _m_datas.size();
+        return _M_datas.size();
     }
 
     size_type max_size() const noexcept
     {
-        return _m_max_size;
+        return _M_max_size;
     }
 
     void clear() noexcept
     {
-        _m_datas.clear();
+        _M_datas.clear();
     }
 
     void resize(size_type __n)
     {
-        _m_max_size = __n;
-        while (_m_datas.size() > _m_max_size)
-            _m_datas.pop_back();
+        _M_max_size = __n;
+        while (_M_datas.size() > _M_max_size)
+            _M_datas.pop_back();
     }
 
     bool empty() const noexcept
     {
-        return _m_datas.empty();
+        return _M_datas.empty();
     }
 
 private:
-    datas_type _m_datas;
-    size_type _m_max_size;
+    datas_type _M_datas;
+    size_type _M_max_size;
 };
 
 #endif // RECENT_HPP
