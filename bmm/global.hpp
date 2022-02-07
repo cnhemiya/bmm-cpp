c8ea8c5e375a43320fd206724cc19c59b79d7a00
cnhemiya <cnhemiya@qq.com>
2022-02-07 14:50:29 +0800

    添加对不支持 C++17 标准的编译器的编译警告

--- bmm/global.hpp
+++ bmm/global.hpp
@@ -9,6 +9,10 @@
 #ifndef _BMM_GLOBAL_HPP_
 #define _BMM_GLOBAL_HPP_
 
+#if __cplusplus < 201703L
+#error "请更换支持 C++ 17 标准的 C++ 编译器。Please replace the C++ compiler that supports the C++ 17 standard"
+#endif
+
 #ifndef _BMM_NAMESPACE_BEGIN_
 #define _BMM_NAMESPACE_BEGIN_ namespace bmm {
 #endif
